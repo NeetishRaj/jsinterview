dd7e194325b10861a8ee6e637d308484476b58fc
dev <dev@smarttask.io>
2021-12-27 13:00:35 +0530

    js test

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+  Return a frequency hash of answers by question_id
+
+  SAMPLE RETURN DATA: 
+  {
+    13903: {
+      "Yes": 2,
+      "No": 4
+    },
+    39023: {
+      "Pizza": 1,
+      "Burrito": 4
+    }
+  }
+*/
+const data = require('./data.json');
+
+function count_survey_responses(data) {
+ // Right your logic to build the result here
+
+  return result;
+}
+
+
+
+count_survey_responses(data);
+
+
+module.exports = { count_survey_responses };
